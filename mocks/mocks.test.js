18e7cb275ed5e6831eea719938ef77ec623bedb3
Jon Taylor <95875505+JWT890@users.noreply.github.com>
2023-10-30 10:45:20 -0400

    Add files via upload

---
+++ mocks/mocks.test.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+
+const fetchData = async (id) => {
+	const results = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        console.log(results)
+        return results.data
+}
+
+module.exports = fetchData;
+
+const forEach = (items, callback) => {
+    for(let i = 0; i < items.length; i++){
+        callback(items[i])
+    }
+}
+
+const mock = (x) => 42 + x
+
+it("mock callback", () => {
+    const mockCalledback = jest.fn(x => 42 + x)
+
+    forEach([0,1], mockCalledback);
+
+    expect(mockCalledback.mock.calls.length).toBe(2)
+    // [ call1, call2 ]
+
+    expect(mockCalledback.mock.calls[0][0]).toBe(0)
+
+    expect(mockCalledback.mock.calls[1][0]).toBe(1)
+
+    expect(mockCalledback.mock.results[1].value).toBe(43)
+})
+
+it("mock return", () => {
+    const mock = jest.fn();
+
+    mock.mockReturnValueOnce(true).mockReturnValueOnce(false).mockReturnValueOnce("hello");
+    const results = mock();
+    const results2 = mock();
+    const results3 = mock();
+
+    expect(results).toBe(true)
+    expect(results2).toBe(false)
+    expect(results3).toBe("hello")
+})
+
+it("mock axios", async () => {
+    jest.spyOn(axios, "get").mockReturnValueOnce({
+        data: {
+            id: 1,
+            todo: "Get 1M Subs"
+        }
+    })
+    const results = await fetchData(1);
+
+    expect(results.todo).toBe("Get 1M Subs")
+})
